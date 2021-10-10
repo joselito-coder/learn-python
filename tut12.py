9575a3388f1f0c94b91d88660a491612b426bffe
Joselito <joselitorebello20@gmail.com>
2021-10-10 12:12:21 +0530

    completed tut12

---
+++ tut12.py
@@ -0,0 +1,41 @@
+var1 = 6
+var2 = 56
+
+# var3 = int(input())
+# if var3 > var2:
+#     print("Greater")
+# elif var3 == var2:
+#     print("Equal")
+# else:
+#     print("Lesser")
+
+# list1 = [5,7,3]
+# print(15 not in list1)
+# if 15 not in list1:
+#     print("No it is not inside the list")
+
+# # Quiz solution
+# print("Enter age: ")
+# age = int(input())
+
+# if age > 18:
+#     print("You can Drive")
+# elif age == 18:
+#     print("Please come offline so we can verify if you can drive")
+# else:
+#     print("You cannot drive")
+
+# Quiz solution
+# print("Enter age: ")
+# age = int(input())
+
+if age > 100:
+    print("Invalid age")
+elif age > 18:
+    print("You can Drive")
+elif age == 18:
+    print("Please come offline so we can verify if you can drive")
+elif age < 7:
+    print("Invalid age")
+else:
+    print("You cannot drive")
