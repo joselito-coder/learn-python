32eda0dcd13717b737fe0d0be1873b114ba97448
Joselito <joselitorebello20@gmail.com>
2021-10-12 06:59:46 +0530

    completed tut21

---
+++ tut21.py
@@ -0,0 +1,45 @@
+# Operators in python
+# Arithmetic operator
+# Assignment operator
+# Comparision operator
+# Logical operators
+# Identity operators
+# Membership operators
+# Bitwise operators
+
+# Arithmetic operators
+# print("5 * 5 is",5 * 5)
+# print("5 - 5 is",5 - 5)
+# print("5 + 5 is",5 + 5)
+# print("5 / 5 is",5 / 5)
+# print("5 % 5 is",5 % 5)
+# print("5 // 5 is",5 // 5)
+# print("5 ** 5 is",5 ** 5)
+
+# Assignment Operators
+# x = 5
+# print(x)
+# x  %= 7 # x = x % 7
+# print(x)
+
+# comparison operators
+# i = 5
+# print(  i == 5)
+
+# Logical operators
+# a = True
+# b = False
+# print(a and b)
+# print(a or b)
+
+# Identity operators
+# print( 5 is not 5)
+
+# Membership Operator
+# list = [3, 77, 1, 23, 212, 23, 23, 1, 2]
+# print(32 not in list)
+
+
+# Bitwise operators
+print( 0 & 2)
+print( 0 | 3 )
