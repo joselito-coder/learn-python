e52c381757cef877b9b10f2578705ec7b5ca3603
Joselito <joselitorebello20@gmail.com>
2021-10-12 07:10:42 +0530

    Completed tut22.py

---
+++ tut22.py
@@ -0,0 +1,6 @@
+a = int(input("Enter a\n>> "))
+b = int(input("Enter b\n>> "))
+
+# if a>b : print("A B se bada hai bhai")
+
+print("B A se bada hai bhai") if a < b else print("A B se bada hai bhai")
