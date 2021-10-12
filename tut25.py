a2ebc29e1663af808e24c6cf282b3b2f62f0d5bd
Joselito <joselitorebello20@gmail.com>
2021-10-12 13:56:28 +0530

    Completed tut25

---
+++ tut25.py
@@ -0,0 +1,15 @@
+# FILE IO basics
+
+"""
+"r" - opens a file for reading
+"w" - open a file for writing
+"x" - creates file is not exists
+"a" - add more to a file
+"t" - text mode
+"b" - binary mode
+"+" - Read and write 
+"""
+
+# Question of the day
+# Q. How to acces the docstring of a function named func1() and print it
+# A. We can access the doctring of func1() by using print(func1.__doc__)
