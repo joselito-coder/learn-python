a9bb6339199a355bb03e15f40db4c9e1bfd4581e
Joselito <joselitorebello20@gmail.com>
2021-10-13 06:33:21 +0530

    Completed tut28

---
+++ tut28.py
@@ -0,0 +1,18 @@
+# writing to files
+
+# opening files in write mode
+# f = open('misc_files/hello.txt','w')
+# f.write("Hello I am writing to a file")
+# f.close()
+
+# opening file in append mode
+# f = open('misc_files/hello.txt','a')
+# a = f.write("\nHello I am appending another line to a file")
+# print(a)
+# f.close()
+
+# Opening file in read and write mode
+# f = open('misc_files/hello.txt','r+')
+# print(f.read())
+# f.write("\nHello I am writing this line in read and write mode")
+# f.close()
