2fbe82f16f474f102ddfcec325f8132f3f912e28
Joselito <joselitorebello20@gmail.com>
2021-10-15 07:11:15 +0530

    completed tut36

---
+++ tut36.py
@@ -0,0 +1,19 @@
+# Lambda function and Anonymous Functions
+
+# def add(a,b):
+#     return a+b
+
+# minus = lambda x,y: x-y
+
+# # def minus(x,y):
+# #     return x-y
+
+# print(minus(99,5))
+
+# def a_first(a):
+#     return a[1]
+
+a = [[1,22],[4,12],[2,3]]
+a.sort(key=lambda x: x[1])
+
+print(a,)
