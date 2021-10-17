6faad69b365657c5c696cf6e4bc14320307e166d
Joselito <joselitorebello20@gmail.com>
2021-10-17 14:20:22 +0530

    completed tut46

---
+++ python_files/tut46/tutmain1.py
@@ -0,0 +1,10 @@
+
+def printharry(str):
+    return f"Yeah string Harry ko de de thakur {str}"
+
+def add (a,b):
+    return a + b + 2
+
+if __name__ == "__main__":
+    print(printharry('str'))
+    print(add(10, 10))
---
+++ python_files/tut46/tutmain2.py
@@ -0,0 +1,3 @@
+import tutmain1
+
+print(tutmain1.printharry('thisis string'))
