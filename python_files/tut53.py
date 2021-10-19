88faec495857ffce4b7587d1506fc9ba7ad531ac
Joselito <joselitorebello20@gmail.com>
2021-10-19 10:13:41 +0530

    Completed tut 53

---
+++ python_files/tut53.py
@@ -0,0 +1,16 @@
+class Student():
+    pass
+
+harry = Student()
+larry = Student()
+
+# print(harry,larry)
+
+harry.name = "Harry"
+harry.std = 12
+harry.section = 1
+
+larry.std = 9
+larry.subjects = ['Hindi','Physics','Maths']
+
+print(harry.name,larry.subjects)
