9a503b5a40c22f5917b2078ab075f1669fc618ff
Joselito <joselitorebello20@gmail.com>
2021-10-27 07:53:32 +0530

    completed tut79

---
+++ python_files/tut79.py
@@ -0,0 +1,22 @@
+import os
+
+# print(dir(os))
+# print(os.getcwd())
+# os.chdir("/home/demon")
+# print(os.getcwd())
+
+# f = open('hello.txt')
+# f.close()
+
+# print(os.listdir(".."))
+# os.makedirs('testDirs/this/that/babu')
+# os.rename('testDirs', 'realDirs')
+
+# os.rmdir('realDirs/')
+
+print(os.environ.get("HOME"))
+
+print(os.path.join("this","is","a","path_that_will","join"))
+
+print(os.path.exists('hello.txt'))
+print(os.path.isfile('tut79.py'))
