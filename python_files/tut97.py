f583aa06f9be6620da808326bc90e0b01b6a0545
Joselito <joselitorebello20@gmail.com>
2021-11-04 08:32:23 +0530

    completed tut97

---
+++ python_files/tut97.py
@@ -0,0 +1,17 @@
+# Learning path for web development
+
+# Web development using python CWH playlist:
+# https://www.youtube.com/playlist?list=PLu0W_9lII9agAiWp6Y41ueUKx1VcTRxmf
+
+# What to learn?
+    # *All basics are covered in the web development with python playlist* 
+    # What is web development?
+    # clients and servers
+
+# Flask vs Django
+# If you want speed and you want to build something from ground level and you want full control you should use flask
+#  If you want something  of "Kitchen Sink" type (https://idioms.thefreedictionary.com/everything+but+the+kitchen+sink) which means you want everything prebuilt, or if you want something prebuilt and want to build a website quickly, Then use Django (slower compared to flask)
+
+# Why web dev using python?
+# Because python has some prebuilt libraries like tensor flow , tensorflow hub and you don't have to write from scratch also you don't need to learn other backend languages like javascript or php
+# You can also create quick apis using python eg. api giving data on Indian railways and returning response in json
