0ec8fb9fc6d8ec2db151892d719730a782f69190
Joselito <joselitorebello20@gmail.com>
2021-11-04 09:14:18 +0530

    completed tut99

---
+++ python_files/tut99.py
@@ -0,0 +1,16 @@
+# Data Science playlist CWH link
+# https://www.youtube.com/playlist?list=PLu0W_9lII9agK8pojo23OHiNz3Jm6VQCH
+# Data science path way : 
+# 1. Numpy  
+# 2. Panda 
+# 3. Python mysql / posgresql 
+# 4. Hadoop python 
+# 5. Dockerizing 
+
+# Machine learning playlist CWH
+# https://www.youtube.com/playlist?list=PLu0W_9lII9ai6fAMHp-acBmJONT7Y4BSG
+
+# Machine learning:
+# TensorFlow
+# TensorFlow hub
+# Sklearn
