babe5534b42054d470c287e17a8db689adf9603c
Joselito <joselitorebello20@gmail.com>
2021-11-04 08:52:38 +0530

    completed tut98

---
+++ python_files/tut98.py
@@ -0,0 +1,11 @@
+# GUI development roadmap
+
+# GUI developemnt playlist link CWH 
+# https://www.youtube.com/playlist?list=PLu0W_9lII9ajLcqRcj4PoEihkukF_OTzA
+
+# Use a gui toolkit 
+# Your options are:
+# 1. tkinter
+# 2. wxPython
+# 3. PyQt
+
