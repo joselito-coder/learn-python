fa5fde1ad74289ffb9080ea665ea79c572004bf1
joselito <joselitorebello20@gmail.com>
2021-12-28 10:44:04 +0530

    Completed my first django app

--- django_tut/hello/home/views.py
+++ django_tut/hello/home/views.py
@@ -1,5 +1,11 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render,HttpResponse
+from datetime import datetime
+from home.models import Contact
+# from django.contrib.messages import constants as messages
+from django.contrib import messages
+
+
 
 # Create your views here.
 
@@ -8,19 +14,25 @@ def index(request):
         "variable":" This is mai beriable",
         "var2":"this is variable 2"
     }
-    
     return render(request,'index.html',context)
 
 def about(request):
-    # return HttpResponse("this is about page")
     return render(request,'about.html')
 
 
 def services(request):
-    # return HttpResponse("this is services page")
     return render(request,'services.html')
 
 
 def contact(request):
-    # return HttpResponse("this is contact page")
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        date = datetime.today()
+        contact = Contact(name=name, email=email, phone=phone,desc=desc,date=date)
+        contact.save()
+        messages.success(request, 'Your message has been sent.')
+
     return render(request,'contact.html')
