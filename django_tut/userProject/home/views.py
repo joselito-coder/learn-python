d54f9f4fe3d1b562bcff1b5889adfc921b952960
joselito <joselitorebello20@gmail.com>
2022-01-03 22:16:55 +0530

    Almost completed login app

---
+++ django_tut/userProject/home/views.py
@@ -0,0 +1,34 @@
+from django.contrib import auth
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate,login,logout
+
+
+# Create your views here.
+def index(request):
+    if request.user.is_anonymous:
+        return redirect('/login')
+    return render(request,'index.html')
+
+def loginUser(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request,username=username,password=password)
+        print("Printing Details: ")
+        print(username)
+        print(password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('/')
+        else:
+            return redirect('/login')
+
+    return render(request,'login.html')
+
+def logoutUser(request):
+    logout(request)
+
+    # return redirect(request,'login.html')
+    return redirect('/login')
+
